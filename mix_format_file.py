026c95e1e2f6992933cda85b27625ad103fea9ed
José Valim <jose.valim@plataformatec.com.br>
2017-10-07 21:25:23 +0200

    Do not use exec but subprocess

--- mix_format_file.py
+++ mix_format_file.py
@@ -1,11 +1,17 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, subprocess
 
 class MixFormatFileCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     window = self.view.window()
     window.run_command("save")
-    window.run_command("exec", {
-      "cmd": ["mix", "format", self.view.file_name()],
-      "working_dir": window.folders()[0]
-    })
-    window.run_command("hide_panel")
+
+    cmd = ["mix", "format", self.view.file_name()]
+    cwd = window.folders()[0]
+    returncode = subprocess.call(cmd, cwd=cwd)
+
+    if returncode == 0:
+      window.status_message("Formatting successful!")
+    else:
+      # TODO: Ideally we would show errors in a pane but life
+      # is too short to figure it out. Pull requests welcome!
+      sublime.error_message("Formatting failed!")
