fcf7b7aeeae809d7b5615b4c759b284a37ff5643
woylie <potoroo@posteo.de>
2018-01-10 16:32:26 +0100

    mix format on save, errors in panel

--- mix_format_file.py
+++ mix_format_file.py
@@ -1,17 +1,54 @@
-import sublime, sublime_plugin, subprocess
+import os
+import sublime
+import sublime_plugin
+import subprocess
 
 class MixFormatFileCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     window = self.view.window()
-    window.run_command("save")
 
-    cmd = ["mix", "format", self.view.file_name()]
-    cwd = window.folders()[0]
-    returncode = subprocess.call(cmd, cwd=cwd)
+    # Hide the console window on Windows
+    shell = False
+    path_separator = ':'
+    if os.name == 'nt':
+        shell = True
+        path_separator = ';'
 
-    if returncode == 0:
-      window.status_message("Formatting successful!")
+    cmd = ['mix', 'format', self.view.file_name()]
+
+    p = subprocess.Popen(
+      cmd,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      shell=shell
+    )
+
+    _, stderrdata = p.communicate()
+
+    stderrdata = stderrdata.strip()
+
+    if stderrdata:
+      panel_view = window.create_output_panel('mix_format')
+      panel_view.set_read_only(False)
+
+      panel_view.run_command('erase_view')
+      panel_view.run_command('append', {'characters': stderrdata.decode()})
+
+      panel_view.set_read_only(True)
+      window.run_command('show_panel', {'panel': 'output.mix_format'})
     else:
-      # TODO: Ideally we would show errors in a pane but life
-      # is too short to figure it out. Pull requests welcome!
-      sublime.error_message("Formatting failed!")
+      window.run_command('hide_panel', {'panel': 'output.mix_format'})
+      window.status_message('Formatting successful!')
+
+
+class MixFormatOnSave(sublime_plugin.EventListener):
+  def on_post_save(self, view):
+    sel = view.sel()[0]
+    region = view.word(sel)
+    scope = view.scope_name(region.b)
+
+    if scope.find('source.elixir') != -1:
+      settings = sublime.load_settings('Elixir.sublime-settings')
+
+      if settings.get('mix_format_on_save', False):
+        view.run_command('mix_format_file')
